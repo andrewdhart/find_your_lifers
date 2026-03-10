18eaf4c95ce660ad5983013138a6ae63364278ce
Andrew Hart <52938128+andrewdhart@users.noreply.github.com>
2026-03-10 08:27:55 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,33 +1,33 @@
-const CACHE="birdchaser-v3"
+const CACHE = "birdchaser-v4";
 
-const FILES=[
-"/",
-"/index.html",
-"/style.css",
-"/app.js",
-"/manifest.json"
-]
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
 
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(CACHE).then(c=>c.addAll(FILES))
-)
-})
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(FILES))
+  );
+});
 
-self.addEventListener("activate",e=>{
-e.waitUntil(
-caches.keys().then(keys=>{
-return Promise.all(
-keys.filter(k=>k!==CACHE)
-.map(k=>caches.delete(k))
-)
-})
-)
-})
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request)
-.then(r=>r || fetch(e.request))
-)
-})
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
